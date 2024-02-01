c8b837b13e8804b63715b8e016da1c28313de41e
Mihail Nemirovchenko <nemirovchenko@gmail.com>
2024-02-01 21:04:36 +0300

    Создал проект C# и перенёс в него дз

---
+++ seminar_02/Program.cs
@@ -0,0 +1,112 @@
+﻿// -------------------
+
+Console.Clear();
+Console.WriteLine(@"Задача 1:
+Напишите программу, которая принимает на вход число и проверяет,
+кратно ли оно одновременно 7 и 23.");
+
+Console.Write("\nВведите число: ");
+int n = int.Parse(Console.ReadLine()!);
+if (n % 7 == 0 && n % 23 == 0)
+    Console.WriteLine($"Число {n} кратно одновременно 7 и 23");
+else
+    Console.WriteLine($"Введённое число ({n}) не кратно 7 и 23");
+
+// -------------------
+
+Console.Clear();
+Console.WriteLine(@"Задача 2:
+Напишите программу, которая принимает на вход координаты точки (X и Y),
+причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости,
+в которой находится эта точка.");
+
+string message = "\nВведите координату ";
+Console.Write(message + "X: ");
+int x = int.Parse(Console.ReadLine()!);
+
+string warning = "Ошибка! Координата не должна быть равна 0.";
+while (x == 0)
+{
+    Console.Write(warning + message + "X: ");
+    x = int.Parse(Console.ReadLine()!);
+}
+
+Console.Write(message + "Y: ");
+int y = int.Parse(Console.ReadLine()!);
+while (y == 0)
+{
+    Console.Write(warning + message + "Y: ");
+    y = int.Parse(Console.ReadLine()!);
+}
+
+string quarter = "";
+if (x > 0 && y > 0)
+    quarter = "I";
+else if (x < 0 && y > 0)
+    quarter = "II";
+else if (x < 0 && y < 0)
+    quarter = "III";
+else if (x > 0 && y < 0)
+    quarter = "IV";
+
+Console.WriteLine($"Координаты ({x}, {y}) принадлежат {quarter} четверти.");
+
+// -------------------
+
+Console.Clear();
+Console.WriteLine(@"Задача 3:
+Напишите программу, которая принимает на вход целое число из отрезка [10, 99]
+и показывает наибольшую цифру числа.");
+
+string message = "\nВведите число: ";
+Console.Write(message);
+int n = int.Parse(Console.ReadLine()!);
+while (n < 10 || n > 99)
+{
+    string warning = "Ошибка! Число должно быть из отрезка [10, 99]";
+    Console.Write(warning + message);
+    n = int.Parse(Console.ReadLine()!);
+}
+
+int n1 = n / 10;
+int n2 = n % 10;
+string max = "";
+if (n1 > n2)
+    max = $"{n1}";
+else if (n1 < n2)
+    max = $"{n2}";
+else
+    max = "Цифры числа равны";
+
+Console.WriteLine(max);
+
+// -------------------
+
+Console.Clear();
+Console.WriteLine(@"Задача 4:
+Напишите программу, которая на вход принимает натуральное число N,
+а на выходе показывает его цифры через запятую.");
+
+string message = "\nВведите число: ";
+Console.Write(message);
+int n = int.Parse(Console.ReadLine()!);
+while (n < 0)
+{
+    string warning = "Число не должно быть отрицательным!";
+    Console.Write(warning + message);
+    n = int.Parse(Console.ReadLine()!);
+}
+
+int num;
+string text = "";
+while (n > 0)
+{
+    num = n % 10;
+    if (n / 10 == 0)
+        text = $"{num}" + text;
+    else
+        text = $", {num}" + text;
+    n /= 10;
+}
+
+Console.WriteLine(text);
