238958981fc1ae8a010a5f46057cb3c31d41c650
nemirmg <156666849+nemirmg@users.noreply.github.com>
2024-02-01 10:31:35 +0300

    задача 3
    Добавил решение задачи 3

--- seminar_02.cs
+++ seminar_02.cs
@@ -58,3 +58,36 @@ else if (x > 0 && y < 0)
     quarter = "IV";
 
 Console.Write($"Координаты ({x}, {y}) принадлежат {quarter} четверти.");
+
+/* -------------------
+Задача 3:
+Напишите программу, которая принимает на вход целое число из отрезка [10, 99]
+и показывает наибольшую цифру числа.
+------------------- */
+
+Console.Clear();
+Console.WriteLine(@"Задача 3:
+Напишите программу, которая принимает на вход целое число из отрезка [10, 99]
+и показывает наибольшую цифру числа.");
+
+string message = "\nВведите число: ";
+Console.Write(message);
+int n = int.Parse(Console.ReadLine()!);
+while (n < 10 || n > 99)
+{
+    string warning = "Ошибка! Число должно быть из отрезка [10, 99]";
+    Console.Write(warning + message);
+    n = int.Parse(Console.ReadLine()!);
+}
+
+int n1 = n / 10;
+int n2 = n % 10;
+string max;
+if (n1 > n2)
+    max = $"{n1}";
+else if (n1 < n2)
+    max = $"{n2}";
+else
+    max = "Цифры числа равны";
+
+Console.WriteLine(max);
