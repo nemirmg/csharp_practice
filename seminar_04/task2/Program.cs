7b6dc01d97166dea90c4d1f1d6b777e478bfd650
Mihail Nemirovchenko <nemirovchenko@gmail.com>
2024-02-10 08:06:42 +0300

    Добавил задачу 2

---
+++ seminar_04/task2/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+Console.WriteLine(@"Задача 2:
+Задайте массив заполненный случайными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел 
+в массиве");
+
+void inputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+}
+
+bool evenNumber(int num)
+{
+    if (num % 2 == 0)
+        return true;
+    return false;
+}
+
+Console.Write("\nВведите количество элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+inputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+
+int count = 0;
+foreach (int element in array)
+{
+    if (evenNumber(element))
+        count++;
+}
+Console.WriteLine($"Чётных чисел в массиве: {count}");
