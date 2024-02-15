2dbb868c689e0aa97a4c0e9f6659e0f787a497d3
Mihail Nemirovchenko <nemirovchenko@gmail.com>
2024-02-15 19:19:27 +0300

    add seminar_06:task4

---
+++ seminar_06/task4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.WriteLine(@"Задача 4*(не обязательная):
+Задайте строку, состоящую из слов, разделенных пробелами. 
+Сформировать строку, в которой слова расположены в обратном порядке. 
+В полученной строке слова должны быть также разделены пробелами.
+");
+
+Console.Write("Введите строку: ");
+string[] str = Console.ReadLine()!.Split();
+string newStr = "";
+for (int i = str.Length - 1; i >= 0; i--){
+    if (str[i] == "")
+        continue;
+    else if (i == 0)
+        newStr += str[i];
+    else
+        newStr += $"{str[i]} ";
+    
+}
+Console.WriteLine(newStr);
+
